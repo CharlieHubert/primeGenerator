1327bb8bb1f979a249d6508461e21d6c5da2c7bf
cjhubert03 <hubertc25@hanover.edu>
2022-09-25 21:38:01 -0400

    remove unneeded fields from Number Printer

--- src/literateProgramming/NumberPrinter.java
+++ src/literateProgramming/NumberPrinter.java
@@ -4,8 +4,6 @@ public class NumberPrinter {
     private final int rowsPerPage;
     private final int columnsPerPage;
     private int pageNumber;
-    private int pageOffset;
-    private int numberOfNumbers;
     private int[] numbers;
 
     public NumberPrinter(int rowsPerPage, int columnsPerPage) {
@@ -22,42 +20,46 @@ public class NumberPrinter {
         }
     }
 
-    private boolean needToPrintMore() {
-        return pageOffset <= numberOfNumbers;
-    }
-
     private void initialize(int[] numbers) {
         pageNumber = 1;
-        pageOffset = 1;
         this.numbers = numbers;
-        numberOfNumbers = this.numbers.length -1;
     }
 
-    private void moveToNextPage() {
-        System.out.println("\f");
-        pageNumber++;
-        pageOffset += rowsPerPage * columnsPerPage;
-    }
-
-    private void printNumbersOnPage() {
-        for (int row = 0; row < rowsPerPage; row++) {
-            for (int col = 0; col < columnsPerPage; col++)
-                printNumberAt(pageOffset + row  + col * rowsPerPage);
-            System.out.println();
-        }
-    }
-
-    private void printNumberAt(int index) {
-        if (index <= numberOfNumbers)
-            System.out.printf("%10d", numbers[index]);
+    private boolean needToPrintMore() {
+        return getPageOffset() <= getNumberOfNumbers();
     }
 
     private void printHeader() {
         System.out.print("The First ");
-        System.out.print(Integer.toString(numberOfNumbers));
+        System.out.print(Integer.toString(getNumberOfNumbers()));
         System.out.print(" Prime Numbers --- Page ");
         System.out.print(Integer.toString(pageNumber));
         System.out.println("\n");
     }
 
+    private void printNumbersOnPage() {
+        for (int row = 0; row < rowsPerPage; row++) {
+            for (int col = 0; col < columnsPerPage; col++)
+                printNumberAt(getPageOffset() + row  + col * rowsPerPage);
+            System.out.println();
+        }
+    }
+
+    private void printNumberAt(int index) {
+        if (index <= getNumberOfNumbers())
+            System.out.printf("%10d", numbers[index]);
+    }
+
+    private void moveToNextPage() {
+        System.out.println("\f");
+        pageNumber++;
+    }
+
+    public int getPageOffset() {
+        return (pageNumber - 1) * rowsPerPage * columnsPerPage + 1;
+    }
+
+    public int getNumberOfNumbers() {
+        return numbers.length -1;
+    }
 }
